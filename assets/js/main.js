eccbbf292203678158d9577ea503e607e022d522
Travis Rodgers <rodgtr1@gmail.com>
2017-01-17 23:00:20 -0500

    added a contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,117 @@ $(function () {
         });
     }
 */
+  
+    // Contact Form
+  [].slice.call( document.querySelectorAll( '.input' ) ).forEach( function( inputEl ) {
+    // in case the input is already filled..
+    if( inputEl.value.trim() !== '' ) {
+      classie.add( inputEl.parentNode, 'input--filled' );
+    }
+
+    // events:
+    inputEl.addEventListener( 'focus', onInputFocus );
+    inputEl.addEventListener( 'blur', onInputBlur );
+  } );
+
+  function onInputFocus( ev ) {
+    classie.add( ev.target.parentNode, 'input--filled' );
+  }
+
+  function onInputBlur( ev ) {
+    if( ev.target.value.trim() === '' ) {
+      classie.remove( ev.target.parentNode, 'input--filled' );
+    }
+  }
+  
+    //Classie.js
+  
+  /*!
+ * classie v1.0.1
+ * class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * MIT license
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+  /*jshint browser: true, strict: true, undef: true, unused: true */
+  /*global define: false, module: false */
+
+  ( function( window ) {
+
+    'use strict';
+
+    // class helper functions from bonzo https://github.com/ded/bonzo
+
+    function classReg( className ) {
+      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+    }
+
+    // classList support for class management
+    // altho to be fair, the api sucks because it won't accept multiple classes at once
+    var hasClass, addClass, removeClass;
+
+    if ( 'classList' in document.documentElement ) {
+      hasClass = function( elem, c ) {
+        return elem.classList.contains( c );
+      };
+      addClass = function( elem, c ) {
+        elem.classList.add( c );
+      };
+      removeClass = function( elem, c ) {
+        elem.classList.remove( c );
+      };
+    }
+    else {
+      hasClass = function( elem, c ) {
+        return classReg( c ).test( elem.className );
+      };
+      addClass = function( elem, c ) {
+        if ( !hasClass( elem, c ) ) {
+          elem.className = elem.className + ' ' + c;
+        }
+      };
+      removeClass = function( elem, c ) {
+        elem.className = elem.className.replace( classReg( c ), ' ' );
+      };
+    }
+
+    function toggleClass( elem, c ) {
+      var fn = hasClass( elem, c ) ? removeClass : addClass;
+      fn( elem, c );
+    }
+
+    var classie = {
+      // full names
+      hasClass: hasClass,
+      addClass: addClass,
+      removeClass: removeClass,
+      toggleClass: toggleClass,
+      // short names
+      has: hasClass,
+      add: addClass,
+      remove: removeClass,
+      toggle: toggleClass
+    };
+
+    // transport
+    if ( typeof define === 'function' && define.amd ) {
+      // AMD
+      define( classie );
+    } else if ( typeof exports === 'object' ) {
+      // CommonJS
+      module.exports = classie;
+    } else {
+      // browser global
+      window.classie = classie;
+    }
+
+  })( window );
+  
     // Fix scrolling issue on Mobile Safari.
     if ($(window).width() < 768) {
         $(window).scrollTop(0);
